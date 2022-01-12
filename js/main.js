3ce875939b970fa910a2f3a66148d68124ce4683
Addy Rodriguez <rdz.33.a@gmail.com>
2022-01-11 23:39:32 -0600

    Stuff Kanye Says

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 console.log("👀")
+
+let url = "https://api.kanye.rest"
+
+const options = {
+    method: 'GET',
+    headers: {}
+}
+
+fetch(url,options).then((resp)=>{
+        console.log(resp)
+        resp.json().then((data)=>{
+            // console.log("quote",data)
+            console.log(data.quote)
+            const kanyeQuotes = data.quote
+
+            document.getElementById("sayings").innerText = kanyeQuotes
+        })
+
+})
+
+function newQuote(){
+    return location.reload()
+}
+
+const stuffKanyeSays =document.querySelector(".stuffKanyeSays")
+console.log(stuffKanyeSays)
+stuffKanyeSays.addEventListener("click",newQuote)
+
+ // console.log(kanyeQuotes)
